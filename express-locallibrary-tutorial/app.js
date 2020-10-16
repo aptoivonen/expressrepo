13e538d96c4560c6ad56d323901b0606c9430853
Aleksi Toivonen <aptoivonen@mail.com>
2020-10-16 15:30:37 +0300

    Update catalog routes

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -5,8 +5,10 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
 var mongoose = require("mongoose");
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var catalogRouter = require("./routes/catalog");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -29,6 +31,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
