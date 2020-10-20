87a9277b4d4f975f19c6d34691e14aa39813bd2f
Aleksi Toivonen <aptoivonen@mail.com>
2020-10-20 11:58:18 +0300

    Add compression middleware

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
 var mongoose = require("mongoose");
+var compression = require("compression");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -27,6 +28,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
