cb1ab8bb8ff84a22e5f7cc4190eb3ab1ac9c6729
Aleksi Toivonen <aptoivonen@mail.com>
2020-10-20 12:01:44 +0300

    Add helmet middleware

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 require("dotenv").config();
 var mongoose = require("mongoose");
 var compression = require("compression");
+var helmet = require("helmet");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -29,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
